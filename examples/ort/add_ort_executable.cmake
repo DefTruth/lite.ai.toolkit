ee2e4d5f5946e4cab428ec052191d1c8c75e9041
DefTruth <qiustudent_r@163.com>
2021-06-25 00:33:55 +0800

    update cmake files.

--- examples/ort/add_ort_executable.cmake
+++ examples/ort/add_ort_executable.cmake
@@ -2,7 +2,7 @@ function(add_ort_executable executable_name field)
     if (NOT ${THIRDPARTY_SET_STATE})
         include(${CMAKE_SOURCE_DIR}/setup_3rdparty.cmake)
     endif()
-    # will be include into CMakeLists.txt at examples/lite
+    # will be include into CMakeLists.txt at examples/ort
     add_executable(${executable_name} ${field}/test_${executable_name}.cpp)
     target_link_libraries(${executable_name} litehub)  # link liblitehub
     set(EXECUTABLE_OUTPUT_PATH ${CMAKE_SOURCE_DIR}/build/litehub/bin)
