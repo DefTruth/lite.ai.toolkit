79b86614db7660180f471cc00cfa529c5a0add1d
DefTruth <qiustudent_r@163.com>
2021-07-06 23:52:57 +0800

    add FaceNet and test ok !

--- ort/core/ort_core.h
+++ ort/core/ort_core.h
@@ -11,39 +11,47 @@
 
 namespace ortcv
 {
-  class LITEHUB_EXPORTS FSANet;              // [0] * reference: https://github.com/omasaht/headpose-fsanet-pytorch
-  class LITEHUB_EXPORTS PFLD;                // [1] * reference: https://github.com/Hsintao/pfld_106_face_landmarks
-  class LITEHUB_EXPORTS UltraFace;           // [2] * reference: https://github.com/Linzaer/Ultra-Light-Fast-Generic-Face-Detector-1MB
-  class LITEHUB_EXPORTS AgeGoogleNet;        // [3] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
-  class LITEHUB_EXPORTS GenderGoogleNet;     // [4] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
-  class LITEHUB_EXPORTS EmotionFerPlus;      // [5] * reference: https://github.com/onnx/models/blob/master/vision/body_analysis/emotion_ferplus
-  class LITEHUB_EXPORTS VGG16Age;            // [6] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
-  class LITEHUB_EXPORTS VGG16Gender;         // [7] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
-  class LITEHUB_EXPORTS SSRNet;              // [8] * reference: https://github.com/oukohou/SSR_Net_Pytorch
-  class LITEHUB_EXPORTS FastStyleTransfer;   // [9] * reference: https://github.com/onnx/models/blob/master/vision/style_transfer/fast_neural_style
-  class LITEHUB_EXPORTS ArcFaceResNet;       // [10] * reference: https://github.com/deepinsight/insightface/tree/master/recognition/arcface_torch
-  class LITEHUB_EXPORTS Colorizer;           // [11] * reference: https://github.com/richzhang/colorization
-  class LITEHUB_EXPORTS SubPixelCNN;         // [12] * reference: https://github.com/niazwazir/SUB_PIXEL_CNN
-  class LITEHUB_EXPORTS YoloV4;              // [13] * reference: https://github.com/argusswift/YOLOv4-pytorch
-  class LITEHUB_EXPORTS YoloV5;              // [14] * reference: https://github.com/ultralytics/yolov5
-  class LITEHUB_EXPORTS YoloV3;              // [15] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/yolov3
-  class LITEHUB_EXPORTS EfficientNetLite4;   // [16] * reference: https://github.com/onnx/models/blob/master/vision/classification/efficientnet-lite4
-  class LITEHUB_EXPORTS ShuffleNetV2;        // [17] * reference: https://github.com/onnx/models/blob/master/vision/classification/shufflenet
-  class LITEHUB_EXPORTS TinyYoloV3;          // [18] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/tiny-yolov3
-  class LITEHUB_EXPORTS SSD;                 // [19] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/ssd
-  class LITEHUB_EXPORTS SSDMobileNetV1;      // [20] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/ssd-mobilenetv1
-  class LITEHUB_EXPORTS DeepLabV3ResNet101;  // [21] * reference: https://pytorch.org/hub/pytorch_vision_deeplabv3_resnet101/
-  class LITEHUB_EXPORTS DenseNet;            // [22] * reference: https://pytorch.org/hub/pytorch_vision_densenet/
-  class LITEHUB_EXPORTS FCNResNet101;        // [23] * reference: https://pytorch.org/hub/pytorch_vision_fcn_resnet101/
-  class LITEHUB_EXPORTS GhostNet;            // [24] * reference：https://pytorch.org/hub/pytorch_vision_ghostnet/
-  class LITEHUB_EXPORTS HdrDNet;             // [25] * reference: https://pytorch.org/hub/pytorch_vision_hardnet/
-  class LITEHUB_EXPORTS IBNNet;              // [26] * reference: https://pytorch.org/hub/pytorch_vision_ibnnet/
-  class LITEHUB_EXPORTS MobileNetV2;         // [27] * reference: https://pytorch.org/hub/pytorch_vision_mobilenet_v2/
-  class LITEHUB_EXPORTS ResNet;              // [28] * reference: https://pytorch.org/hub/pytorch_vision_resnet/
-  class LITEHUB_EXPORTS ResNeXt;             // [29] * reference: https://pytorch.org/hub/pytorch_vision_resnext/
-  class LITEHUB_EXPORTS GlintCosFace;        // [30] * reference: https://github.com/deepinsight/insightface/tree/master/recognition/arcface_torch
-  class LITEHUB_EXPORTS GlintPartialFC;      // [31] * reference: https://github.com/deepinsight/insightface/tree/master/recognition/partial_fc
-  class LITEHUB_EXPORTS FaceNet;             // [32] * reference: https://github.com/timesler/facenet-pytorch
+  class LITEHUB_EXPORTS FSANet;                     // [0] * reference: https://github.com/omasaht/headpose-fsanet-pytorch
+  class LITEHUB_EXPORTS PFLD;                       // [1] * reference: https://github.com/Hsintao/pfld_106_face_landmarks
+  class LITEHUB_EXPORTS UltraFace;                  // [2] * reference: https://github.com/Linzaer/Ultra-Light-Fast-Generic-Face-Detector-1MB
+  class LITEHUB_EXPORTS AgeGoogleNet;               // [3] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
+  class LITEHUB_EXPORTS GenderGoogleNet;            // [4] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
+  class LITEHUB_EXPORTS EmotionFerPlus;             // [5] * reference: https://github.com/onnx/models/blob/master/vision/body_analysis/emotion_ferplus
+  class LITEHUB_EXPORTS VGG16Age;                   // [6] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
+  class LITEHUB_EXPORTS VGG16Gender;                // [7] * reference: https://github.com/onnx/models/tree/master/vision/body_analysis/age_gender
+  class LITEHUB_EXPORTS SSRNet;                     // [8] * reference: https://github.com/oukohou/SSR_Net_Pytorch
+  class LITEHUB_EXPORTS FastStyleTransfer;          // [9] * reference: https://github.com/onnx/models/blob/master/vision/style_transfer/fast_neural_style
+  class LITEHUB_EXPORTS ArcFaceResNet;              // [10] * reference: https://github.com/deepinsight/insightface/tree/master/recognition/arcface_torch
+  class LITEHUB_EXPORTS Colorizer;                  // [11] * reference: https://github.com/richzhang/colorization
+  class LITEHUB_EXPORTS SubPixelCNN;                // [12] * reference: https://github.com/niazwazir/SUB_PIXEL_CNN
+  class LITEHUB_EXPORTS YoloV4;                     // [13] * reference: https://github.com/argusswift/YOLOv4-pytorch
+  class LITEHUB_EXPORTS YoloV5;                     // [14] * reference: https://github.com/ultralytics/yolov5
+  class LITEHUB_EXPORTS YoloV3;                     // [15] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/yolov3
+  class LITEHUB_EXPORTS EfficientNetLite4;          // [16] * reference: https://github.com/onnx/models/blob/master/vision/classification/efficientnet-lite4
+  class LITEHUB_EXPORTS ShuffleNetV2;               // [17] * reference: https://github.com/onnx/models/blob/master/vision/classification/shufflenet
+  class LITEHUB_EXPORTS TinyYoloV3;                 // [18] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/tiny-yolov3
+  class LITEHUB_EXPORTS SSD;                        // [19] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/ssd
+  class LITEHUB_EXPORTS SSDMobileNetV1;             // [20] * reference: https://github.com/onnx/models/blob/master/vision/object_detection_segmentation/ssd-mobilenetv1
+  class LITEHUB_EXPORTS DeepLabV3ResNet101;         // [21] * reference: https://pytorch.org/hub/pytorch_vision_deeplabv3_resnet101/
+  class LITEHUB_EXPORTS DenseNet;                   // [22] * reference: https://pytorch.org/hub/pytorch_vision_densenet/
+  class LITEHUB_EXPORTS FCNResNet101;               // [23] * reference: https://pytorch.org/hub/pytorch_vision_fcn_resnet101/
+  class LITEHUB_EXPORTS GhostNet;                   // [24] * reference：https://pytorch.org/hub/pytorch_vision_ghostnet/
+  class LITEHUB_EXPORTS HdrDNet;                    // [25] * reference: https://pytorch.org/hub/pytorch_vision_hardnet/
+  class LITEHUB_EXPORTS IBNNet;                     // [26] * reference: https://pytorch.org/hub/pytorch_vision_ibnnet/
+  class LITEHUB_EXPORTS MobileNetV2;                // [27] * reference: https://pytorch.org/hub/pytorch_vision_mobilenet_v2/
+  class LITEHUB_EXPORTS ResNet;                     // [28] * reference: https://pytorch.org/hub/pytorch_vision_resnet/
+  class LITEHUB_EXPORTS ResNeXt;                    // [29] * reference: https://pytorch.org/hub/pytorch_vision_resnext/
+  class LITEHUB_EXPORTS GlintCosFace;               // [30] * reference: https://github.com/deepinsight/insightface/tree/master/recognition/arcface_torch
+  class LITEHUB_EXPORTS GlintPartialFC;             // [31] * reference: https://github.com/deepinsight/insightface/tree/master/recognition/partial_fc
+  class LITEHUB_EXPORTS FaceNet;                    // [32] * reference: https://github.com/timesler/facenet-pytorch
+  class LITEHUB_EXPORTS FocalIRArcFace;             // [33] reference: https://github.com/ZhaoJ9014/face.evoLVe.PyTorch
+  class LITEHUB_EXPORTS FocalIRAsiaArcFace;         // [34] reference: https://github.com/ZhaoJ9014/face.evoLVe.PyTorch
+  class LITEHUB_EXPORTS TencentCurricularFace;      // [35] reference: https://github.com/Tencent/TFace/tree/master/tasks/distfc
+  class LITEHUB_EXPORTS TencentCifpFace;            // [36] reference: https://github.com/Tencent/TFace/tree/master/tasks/cifp
+  class LITEHUB_EXPORTS CenterLossFace;             // [37] reference: https://github.com/louis-she/center-loss.pytorch
+  class LITEHUB_EXPORTS SphereFace;                 // [38] reference: https://github.com/wy1iu/sphereface
+  class LITEHUB_EXPORTS PoseRobustFace;             // [39] reference: https://github.com/penincillin/DREAM
+  class LITEHUB_EXPORTS MobileFaceNet;              // [40] reference: https://github.com/Xiaoccer/MobileFaceNet_Pytorch
 }
 
 namespace ortnlp
