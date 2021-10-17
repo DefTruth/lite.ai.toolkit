09ee7bd8775c9aaf8b35796178baa0ce7d30f6b8
DefTruth <qiustudent_r@163.com>
2021-10-17 12:57:43 +0800

    add ENABLE_XXX options to build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,10 @@ else
   rm -rf ./build/* && echo "clear built files done ! & rebuilding ..."
 fi
 
-cd build && cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DENABLE_MNN=OFF -DENABLE_NCNN=OFF .. && make -j8
+cd build && cmake \
+  -DCMAKE_BUILD_TYPE=MinSizeRel \
+  -DINCLUDE_OPENCV=ON \
+  -DENABLE_MNN=OFF \
+  -DENABLE_NCNN=OFF \
+  -DENABLE_TNN=OFF \
+  .. && make -j8
--- lite/tnn/core/tnn_config.h
+++ lite/tnn/core/tnn_config.h
@@ -5,4 +5,17 @@
 #ifndef LITE_AI_TOOLKIT_TNN_CORE_TNN_CONFIG_H
 #define LITE_AI_TOOLKIT_TNN_CORE_TNN_CONFIG_H
 
+#include "tnn_defs.h"
+#include "lite/lite.ai.headers.h"
+
+#ifdef ENABLE_TNN
+
+#include "tnn/core/macro.h"
+#include "tnn/core/tnn.h"
+#include "tnn/utils/blob_converter.h"
+#include "tnn/utils/mat_utils.h"
+#include "tnn/utils/dims_vector_utils.h"
+
+#endif
+
 #endif //LITE_AI_TOOLKIT_TNN_CORE_TNN_CONFIG_H
