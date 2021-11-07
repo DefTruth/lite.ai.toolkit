bed940702167c5907a389a9a5c6f4d99a22e3a84
DefTruth <qiustudent_r@163.com>
2021-11-07 14:01:33 +0800

    Delete unnecessary libs

--- cmake/setup_opencv_libs.cmake
+++ cmake/setup_opencv_libs.cmake
@@ -1,5 +1,11 @@
 # 1. setup 3rd-party dependencies
 message("########## Setting up OpenCV libs for: ${CMAKE_CURRENT_SOURCE_DIR} ###########")
-set(OpenCV_LIBS opencv_highgui opencv_core opencv_imgcodecs opencv_imgproc opencv_video opencv_videoio) # need only
+set(OpenCV_LIBS
+        opencv_core
+        opencv_imgproc
+        opencv_imgcodecs
+        opencv_video
+        opencv_videoio
+        ) # need only
 message("###########################################################################################")
 
