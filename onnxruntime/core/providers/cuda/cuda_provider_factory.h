ae12de8359c1123e3424e3f536afd5961aef21f8
DefTruth <qiustudent_r@163.com>
2021-11-07 17:28:10 +0800

    try fix and add notes for onnxruntime-linux-gpu support(#97)

--- onnxruntime/core/providers/cuda/cuda_provider_factory.h
+++ onnxruntime/core/providers/cuda/cuda_provider_factory.h
@@ -1,7 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "onnxruntime_c_api.h"
+// #include "onnxruntime_c_api.h" // official code of onnxruntime 1.7.0
+// The new include line was update by lite.ai.toolkit at 2021107 to make
+// sure the 'onnxruntime_c_api.h' header can be include into this file
+// correctly.
+#include "onnxruntime/core/session/onnxruntime_c_api.h"
 
 #ifdef __cplusplus
 extern "C" {
