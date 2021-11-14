5beff9b427ffbf0ef661622abd3f13321128495e
DefTruth <qiustudent_r@163.com>
2021-11-14 17:49:42 +0800

    add NCNN/MNN/TNN support for Face Recognition 🚀

--- examples/lite/cv/test_lite_facenet.cpp
+++ examples/lite/cv/test_lite_facenet.cpp
@@ -58,6 +58,27 @@ static void test_onnxruntime()
 static void test_mnn()
 {
 #ifdef ENABLE_MNN
+  std::string mnn_path = "../../../hub/mnn/cv/facenet_casia-webface_resnet.mnn";
+  std::string test_img_path0 = "../../../examples/lite/resources/test_lite_facenet_0.png";
+  std::string test_img_path1 = "../../../examples/lite/resources/test_lite_facenet_2.png";
+
+  lite::mnn::cv::faceid::FaceNet *facenet =
+      new lite::mnn::cv::faceid::FaceNet(mnn_path);
+
+  lite::types::FaceContent face_content0, face_content1;
+  cv::Mat img_bgr0 = cv::imread(test_img_path0);
+  cv::Mat img_bgr1 = cv::imread(test_img_path1);
+  facenet->detect(img_bgr0, face_content0);
+  facenet->detect(img_bgr1, face_content1);
+
+  if (face_content0.flag && face_content1.flag)
+  {
+    float sim = lite::utils::math::cosine_similarity<float>(
+        face_content0.embedding, face_content1.embedding);
+    std::cout << "MNN Version Detected Sim: " << sim << std::endl;
+  }
+
+  delete facenet;
 #endif
 }
 
