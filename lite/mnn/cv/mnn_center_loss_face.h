a5ef3b7190ca7ea7b194944f062acbeb9c91f4b1
DefTruth <qiustudent_r@163.com>
2021-11-14 18:05:46 +0800

    add NCNN/MNN/TNN support for Face Recognition 🚀

--- lite/mnn/cv/mnn_center_loss_face.h
+++ lite/mnn/cv/mnn_center_loss_face.h
@@ -5,4 +5,28 @@
 #ifndef LITE_AI_TOOLKIT_MNN_CV_MNN_CENTER_LOSS_FACE_H
 #define LITE_AI_TOOLKIT_MNN_CV_MNN_CENTER_LOSS_FACE_H
 
+#include "lite/mnn/core/mnn_core.h"
+
+namespace mnncv
+{
+  class LITE_EXPORTS MNNCenterLossFace : public BasicMNNHandler
+  {
+  public:
+    explicit MNNCenterLossFace(const std::string &_mnn_path, unsigned int _num_threads = 1); //
+    ~MNNCenterLossFace() override = default;
+
+  private:
+    const float mean_vals[3] = {127.5f, 127.5f, 127.5f}; // RGB
+    const float norm_vals[3] = {1.f / 127.5f, 1.f / 127.5f, 1.f / 127.5f};
+
+  private:
+    void initialize_pretreat(); //
+
+    void transform(const cv::Mat &mat) override; //
+
+  public:
+    void detect(const cv::Mat &mat, types::FaceContent &face_content);
+  };
+}
+
 #endif //LITE_AI_TOOLKIT_MNN_CV_MNN_CENTER_LOSS_FACE_H
