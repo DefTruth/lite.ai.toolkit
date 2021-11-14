8d5087ea191b5a442ccdf719a11140a2e940899c
DefTruth <qiustudent_r@163.com>
2021-11-14 17:35:59 +0800

    add NCNN/MNN/TNN support for Face Recognition 🚀

--- examples/lite/cv/test_lite_glint_partial_fc.cpp
+++ examples/lite/cv/test_lite_glint_partial_fc.cpp
@@ -58,6 +58,27 @@ static void test_onnxruntime()
 static void test_mnn()
 {
 #ifdef ENABLE_MNN
+  std::string mnn_path = "../../../hub/mnn/cv/partial_fc_glint360k_r50.mnn";
+  std::string test_img_path0 = "../../../examples/lite/resources/test_lite_glint_partial_fc_0.png";
+  std::string test_img_path1 = "../../../examples/lite/resources/test_lite_glint_partial_fc_2.png";
+
+  lite::mnn::cv::faceid::GlintPartialFC *glint_partial_fc =
+      new lite::mnn::cv::faceid::GlintPartialFC(mnn_path);
+
+  lite::types::FaceContent face_content0, face_content1;
+  cv::Mat img_bgr0 = cv::imread(test_img_path0);
+  cv::Mat img_bgr1 = cv::imread(test_img_path1);
+  glint_partial_fc->detect(img_bgr0, face_content0);
+  glint_partial_fc->detect(img_bgr1, face_content1);
+
+  if (face_content0.flag && face_content1.flag)
+  {
+    float sim = lite::utils::math::cosine_similarity<float>(
+        face_content0.embedding, face_content1.embedding);
+    std::cout << "MNN Version Detected Sim: " << sim << std::endl;
+  }
+
+  delete glint_partial_fc;
 #endif
 }
 
--- lite/mnn/cv/mnn_glint_cosface.h
+++ lite/mnn/cv/mnn_glint_cosface.h
@@ -5,4 +5,29 @@
 #ifndef LITE_AI_TOOLKIT_MNN_CV_MNN_GLINT_COSFACE_H
 #define LITE_AI_TOOLKIT_MNN_CV_MNN_GLINT_COSFACE_H
 
+#include "lite/mnn/core/mnn_core.h"
+
+namespace mnncv
+{
+  class LITE_EXPORTS MNNGlintCosFace : public BasicMNNHandler
+  {
+  public:
+    explicit MNNGlintCosFace(const std::string &_mnn_path, unsigned int _num_threads = 1); //
+    ~MNNGlintCosFace() override = default;
+
+  private:
+    const float mean_vals[3] = {127.5f, 127.5f, 127.5f}; // RGB
+    const float norm_vals[3] = {1.f / 127.5f, 1.f / 127.5f, 1.f / 127.5f};
+
+  private:
+    void initialize_pretreat(); //
+
+    void transform(const cv::Mat &mat) override; //
+
+  public:
+    void detect(const cv::Mat &mat, types::FaceContent &face_content);
+  };
+}
+
+
 #endif //LITE_AI_TOOLKIT_MNN_CV_MNN_GLINT_COSFACE_H
