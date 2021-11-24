b9d34544567d609847c12dde367fb33d47b41839
DefTruth <qiustudent_r@163.com>
2021-11-25 00:03:46 +0800

    add MNN/TNN support for FSANet ✅

---
+++ lite/mnn/cv/mnn_fsanet.h
@@ -0,0 +1,8 @@
+//
+// Created by DefTruth on 2021/11/25.
+//
+
+#ifndef LITE_AI_TOOLKIT_MNN_CV_MNN_FSANET_H
+#define LITE_AI_TOOLKIT_MNN_CV_MNN_FSANET_H
+
+#endif //LITE_AI_TOOLKIT_MNN_CV_MNN_FSANET_H
---
+++ lite/tnn/cv/tnn_fsanet.h
@@ -0,0 +1,8 @@
+//
+// Created by DefTruth on 2021/11/25.
+//
+
+#ifndef LITE_AI_TOOLKIT_TNN_CV_TNN_FSANET_H
+#define LITE_AI_TOOLKIT_TNN_CV_TNN_FSANET_H
+
+#endif //LITE_AI_TOOLKIT_TNN_CV_TNN_FSANET_H
