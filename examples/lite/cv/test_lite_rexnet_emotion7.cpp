1a1d6f8d614d1415e7e2188f9c08b3e1e23db69e
DefTruth <qiustudent_r@163.com>
2021-11-28 19:58:45 +0800

    add MNN/TNN/NCNN support for Face Attributes 🔥

--- examples/lite/cv/test_lite_rexnet_emotion7.cpp
+++ examples/lite/cv/test_lite_rexnet_emotion7.cpp
@@ -56,6 +56,25 @@ static void test_onnxruntime()
 static void test_mnn()
 {
 #ifdef ENABLE_MNN
+  std::string mnn_path = "../../../hub/mnn/cv/face-emotion-recognition-affectnet_7_vggface2_rexnet150.mnn";
+  std::string test_img_path = "../../../examples/lite/resources/test_lite_emotion.jpg";
+  std::string save_img_path = "../../../logs/test_lite_emotion_mnn.jpg";
+
+  lite::mnn::cv::face::attr::ReXNetEmotion7 *rexnet_emotion7 =
+      new lite::mnn::cv::face::attr::ReXNetEmotion7(mnn_path);
+
+  lite::types::Emotions emotions;
+  cv::Mat img_bgr = cv::imread(test_img_path);
+  rexnet_emotion7->detect(img_bgr, emotions);
+
+  lite::utils::draw_emotion_inplace(img_bgr, emotions);
+
+  cv::imwrite(save_img_path, img_bgr);
+
+  std::cout << "MNN Version Detected Emotion: " << emotions.text
+            << ": " << emotions.score << std::endl;
+
+  delete rexnet_emotion7;
 #endif
 }
 
@@ -68,6 +87,26 @@ static void test_ncnn()
 static void test_tnn()
 {
 #ifdef ENABLE_TNN
+  std::string proto_path = "../../../hub/tnn/cv/face-emotion-recognition-affectnet_7_vggface2_rexnet150.opt.tnnproto";
+  std::string model_path = "../../../hub/tnn/cv/face-emotion-recognition-affectnet_7_vggface2_rexnet150.opt.tnnmodel";
+  std::string test_img_path = "../../../examples/lite/resources/test_lite_emotion.jpg";
+  std::string save_img_path = "../../../logs/test_lite_emotion_tnn.jpg";
+
+  lite::tnn::cv::face::attr::ReXNetEmotion7 *rexnet_emotion7 =
+      new lite::tnn::cv::face::attr::ReXNetEmotion7(proto_path, model_path);
+
+  lite::types::Emotions emotions;
+  cv::Mat img_bgr = cv::imread(test_img_path);
+  rexnet_emotion7->detect(img_bgr, emotions);
+
+  lite::utils::draw_emotion_inplace(img_bgr, emotions);
+
+  cv::imwrite(save_img_path, img_bgr);
+
+  std::cout << "TNN Version Detected Emotion: " << emotions.text
+            << ": " << emotions.score << std::endl;
+
+  delete rexnet_emotion7;
 #endif
 }
 
