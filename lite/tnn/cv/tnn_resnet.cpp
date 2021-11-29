9e43e9023abc338a9b2176e8303df00ae775ba7c
DefTruth <qiustudent_r@163.com>
2021-11-29 16:54:56 +0800

    add MNN/TNN/NCNN support for Image Classification 🔥

--- lite/tnn/cv/tnn_resnet.cpp
+++ lite/tnn/cv/tnn_resnet.cpp
@@ -2,3 +2,93 @@
 // Created by DefTruth on 2021/11/29.
 //
 
+#include "tnn_resnet.h"
+#include "lite/utils.h"
+
+using tnncv::TNNResNet;
+
+TNNResNet::TNNResNet(const std::string &_proto_path,
+                     const std::string &_model_path,
+                     unsigned int _num_threads) :
+    BasicTNNHandler(_proto_path, _model_path, _num_threads)
+{
+}
+
+void TNNResNet::transform(const cv::Mat &mat)
+{
+  cv::Mat mat_rs;
+  cv::resize(mat, mat_rs, cv::Size(input_width, input_height));
+  cv::cvtColor(mat_rs, mat_rs, cv::COLOR_BGR2RGB);
+  // push into input_mat (1,3,224,224)
+  input_mat = std::make_shared<tnn::Mat>(input_device_type, tnn::N8UC3,
+                                         input_shape, (void *) mat_rs.data);
+  if (!input_mat->GetData())
+  {
+#ifdef LITETNN_DEBUG
+    std::cout << "input_mat == nullptr! transform failed\n";
+#endif
+  }
+}
+
+void TNNResNet::detect(const cv::Mat &mat, types::ImageNetContent &content, unsigned int top_k)
+{
+  if (mat.empty()) return;
+
+  // 1. make input mat
+  this->transform(mat);
+  // 2. set input_mat
+  tnn::MatConvertParam input_cvt_param;
+  input_cvt_param.scale = scale_vals;
+  input_cvt_param.bias = bias_vals;
+
+  tnn::Status status;
+  status = instance->SetInputMat(input_mat, input_cvt_param);
+  if (status != tnn::TNN_OK)
+  {
+#ifdef LITETNN_DEBUG
+    std::cout << status.description().c_str() << "\n";
+#endif
+    return;
+  }
+  // 3. forward
+  status = instance->Forward();
+  if (status != tnn::TNN_OK)
+  {
+#ifdef LITETNN_DEBUG
+    std::cout << status.description().c_str() << "\n";
+#endif
+    return;
+  }
+  // 4. fetch.
+  tnn::MatConvertParam cvt_param;
+  std::shared_ptr<tnn::Mat> logits_mat; // (1,1000)
+  status = instance->GetOutputMat(logits_mat, cvt_param, "logits", output_device_type);
+
+  if (status != tnn::TNN_OK)
+  {
+#ifdef LITETNN_DEBUG
+    std::cout << status.description().c_str() << "\n";
+#endif
+    return;
+  }
+
+  auto logits_dims = logits_mat->GetDims();
+  const unsigned int num_classes = logits_dims.at(1); // 1000
+  const float *logits = (float *) logits_mat->GetData();
+
+  unsigned int max_id;
+  std::vector<float> scores = lite::utils::math::softmax<float>(logits, num_classes, max_id);
+  std::vector<unsigned int> sorted_indices = lite::utils::math::argsort<float>(scores);
+  if (top_k > num_classes) top_k = num_classes;
+
+  content.scores.clear();
+  content.labels.clear();
+  content.texts.clear();
+  for (unsigned int i = 0; i < top_k; ++i)
+  {
+    content.labels.push_back(sorted_indices[i]);
+    content.scores.push_back(scores[sorted_indices[i]]);
+    content.texts.push_back(class_names[sorted_indices[i]]);
+  }
+  content.flag = true;
+}
