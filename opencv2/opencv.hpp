4f64eaf9339907debc3a610ad4695cd9517ee858
DefTruth <qiustudent_r@163.com>
2022-01-09 11:38:30 +0800

    refactor(OpenCV): pouring lite.ai.toolkit into a singleton lib (#63)

--- opencv2/opencv.hpp
+++ opencv2/opencv.hpp
@@ -43,6 +43,7 @@
 #ifndef OPENCV_ALL_HPP
 #define OPENCV_ALL_HPP
 
+
 // File that defines what modules where included during the build of OpenCV
 // These are purely the defines of the correct HAVE_OPENCV_modulename values
 #include "opencv2/opencv_modules.hpp"
