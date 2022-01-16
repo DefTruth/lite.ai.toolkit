2486db66b61f19f9cf393a16bee70dc183e35905
DefTruth <qiustudent_r@163.com>
2022-01-16 12:15:13 +0800

    feat(toolkit): 🍅 add YOLO5Face MNN/NCNN/TNN/ONNXRuntime (#163)

--- lite/ort/cv/yolo5face.h
+++ lite/ort/cv/yolo5face.h
@@ -5,4 +5,14 @@
 #ifndef LITE_AI_TOOLKIT_ORT_CV_YOLO5FACE_H
 #define LITE_AI_TOOLKIT_ORT_CV_YOLO5FACE_H
 
+#include "lite/ort/core/ort_core.h"
+
+namespace ortcv
+{
+  class LITE_EXPORTS YOLO5Face : public BasicOrtHandler
+  {
+
+  };
+}
+
 #endif //LITE_AI_TOOLKIT_ORT_CV_YOLO5FACE_H
