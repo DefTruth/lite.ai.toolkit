2ae83e1529d75b6bd9051d83064966ce672ec086
DefTruth <qiustudent_r@163.com>
2022-05-31 13:06:10 +0800

    fix(Docs): fixed PIPNet ONNX files wrong order (#304)

--- cmake/lite.ai.toolkit.cmake
+++ cmake/lite.ai.toolkit.cmake
@@ -1,7 +1,6 @@
 add_lite_ai_toolkit_shared_library(${VERSION_STRING} ${SOVERSION_STRING})
 add_lite_ai_toolkit_engines_headers_command()
 
-# TODO: Windows需要之后兼容
 if(${PLATFORM_NAME} MATCHES macos OR ${PLATFORM_NAME} MATCHES linux)
     add_lite_ai_toolkit_engines_libs_command()
 endif()
