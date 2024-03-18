ee89d708751946aeda4bb1dbca474613358159b2
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-03-18 09:52:54 +0800

    Update ncnn.cmake

--- cmake/ncnn.cmake
+++ cmake/ncnn.cmake
@@ -1,6 +1,6 @@
 set(NCNN_DIR ${THIRD_PARTY_PATH}/ncnn)
 if(NOT EXISTS ${NCNN_DIR})
-    message(FATAL_ERROR "${NCNN_DIR} is not exists!")
+    message(FATAL_ERROR "[Lite.AI.Toolkit][E] ${NCNN_DIR} is not exists!")
 endif()
 include_directories(${NCNN_DIR}/include)
 link_directories(${NCNN_DIR}/lib)
@@ -16,14 +16,9 @@ file(GLOB NCNN_CV_HEAD ${CMAKE_SOURCE_DIR}/lite/ncnn/cv/*.h)
 file(GLOB NCNN_NLP_HEAD ${CMAKE_SOURCE_DIR}/lite/ncnn/nlp/*.h)
 file(GLOB NCNN_ASR_HEAD ${CMAKE_SOURCE_DIR}/lite/ncnn/asr/*.h)
 
-set(NCNN_SRCS
-        ${NCNN_CV_SRCS}
-        ${NCNN_NLP_SRCS}
-        ${NCNN_ASR_SRCS}
-        ${NCNN_CORE_SRCS})
-
+set(NCNN_SRCS ${NCNN_CV_SRCS} ${NCNN_NLP_SRCS} ${NCNN_ASR_SRCS} ${NCNN_CORE_SRCS})
 # 3. copy
-message("Installing Lite.AI.ToolKit Headers for NCNN Backend ...")
+message("[Lite.AI.Toolkit][I] Installing Lite.AI.ToolKit Headers for NCNN Backend ...")
 # "INSTALL" can copy all files from the list to the specified path.
 # "COPY" only copies one file to a specified path
 file(INSTALL ${NCNN_CORE_HEAD} DESTINATION ${CMAKE_INSTALL_PREFIX}/include/lite/ncnn/core)
