408b657f6bcacedb3abd40f74381e8f9209531e6
DefTruth <31974251+DefTruth@users.noreply.github.com>
2024-03-18 09:38:22 +0800

    Update lite.ai.toolkit-config.cmake

--- cmake/lite.ai.toolkit-config.cmake
+++ cmake/lite.ai.toolkit-config.cmake
@@ -1,10 +1,10 @@
 #    This file will define the following variables for find_package method:
-#      - Lite_AI_LIBS                 : The list of libraries to link against.
-#      - Lite_AI_INCLUDE_DIRS         : The FastDeploy include directories.
-#      - Lite_AI_Found                : The status of FastDeploy
+#      - lite.ai.toolkit_LIBS                 : The list of libraries to link against.
+#      - lite.ai.toolkit_INCLUDE_DIRS         : The lite.ai.toolkit include directories.
+#      - lite.ai.toolkit_Found                : The status of lite.ai.toolkit
 
 include(${CMAKE_CURRENT_LIST_DIR}/lite.ai.toolkit.cmake)
-# setup FastDeploy cmake variables
-set(Lite_AI_LIBS ${Lite_AI_LIBS})
-set(Lite_AI_INCLUDE_DIRS ${Lite_AI_INCLUDE_DIRS})
-set(Lite_AI_Found TRUE)
+# setup lite.ai.toolkit cmake variables
+set(lite.ai.toolkit_LIBS ${Lite_AI_LIBS})
+set(lite.ai.toolkit_INCLUDE_DIRS ${Lite_AI_INCLUDE_DIRS})
+set(lite.ai.toolkit_Found TRUE)
