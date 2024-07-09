c7a46a82daea70941c60eb1bad25facbc41b21f5
huchenghang <huchenghang@tenorshare.cn>
2024-07-09 14:57:37 +0800

    新增yoloface的测试cpp文件

---
+++ examples/lite/cv/test_lite_yolovfacev8.cpp
@@ -0,0 +1,44 @@
+//
+// Created by ai-test1 on 24-7-8.
+//
+
+#include "lite/lite.h"
+#include "lite/ort/cv/yolofacev8.h"
+
+// 检测并绘制检测框
+void draw_bboxes(cv::Mat& image, const std::vector<lite::types::Bbox>& bboxes) {
+    for (const auto& bbox : bboxes) {
+        cv::rectangle(image, cv::Point(bbox.xmin, bbox.ymin), cv::Point(bbox.xmax, bbox.ymax), cv::Scalar(0, 255, 0), 2);
+    }
+}
+
+static void test_default()
+{
+    std::string onnx_path = "../../hub/onnx/cv/yoloface_8n.onnx";
+    std::string test_img_path = "../resources/test_lite_face_detector_2.jpg";
+    std::string save_img_path = "Lite-Face-Detect.jpg";
+
+    // 1. Test Default Engine ONNXRuntime
+    lite::cv::face::detect::YOLOV8Face *yolov8Face = new lite::cv::face::detect::YOLOV8Face(onnx_path);
+
+    std::vector<lite::types::Bbox> detected_boxes;
+
+    cv::Mat img_bgr = cv::imread(test_img_path);
+
+    yolov8Face->detect(img_bgr, detected_boxes);
+
+    draw_bboxes(img_bgr,detected_boxes);
+
+    cv::imwrite(save_img_path,img_bgr);
+
+    std::cout<<"face detect done!"<<std::endl;
+
+    delete yolov8Face;
+
+}
+
+int main(__unused int argc, __unused char *argv[])
+{
+    test_default();
+    return 0;
+}
