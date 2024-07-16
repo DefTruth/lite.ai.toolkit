1d7714e1e0705e36f60f94bf8a4f58ffabee4220
wangzijian1010 <wangyuyan1022@gmail.com>
2024-07-16 10:24:29 +0800

    add trt test func in cpp file

--- examples/lite/cv/test_lite_yolov8face.cpp
+++ examples/lite/cv/test_lite_yolov8face.cpp
@@ -28,8 +28,34 @@ static void test_default()
 
 }
 
+
+
+static void test_tensorrt()
+{
+#ifdef ENABLE_TENSORRT
+    std::string engine_path = "../../../examples/hub/trt/cv/yoloface_8n.engine";
+    std::string test_img_path = "../../../examples/lite/resources/test_lite_yolov5_1.jpg";
+    std::string save_img_path = "../../../examples/logs/test_lite_yolov8face_trt.jpg";
+    lite::trt::cv::face::detection::YOLOV8Face *yolov8_face  = new lite::trt::cv::face::detection::YOLOV8Face (engine_path);
+
+    cv::Mat test_image = cv::imread(test_img_path);
+
+    std::vector<lite::types::Boxf> detected_boxes;
+
+    yolov8_face->detect(test_image,detected_boxes,0.5f,0.4f);
+
+    cv::imwrite(save_img_path,test_image);
+
+    std::cout<<"trt face detect done!"<<std::endl;
+#endif
+
+}
+
+
+
 int main(__unused int argc, __unused char *argv[])
 {
     test_default();
+    test_tensorrt();
     return 0;
 }
