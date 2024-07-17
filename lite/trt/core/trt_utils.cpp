ce136e4002d4efda99877d3f336928b7a6d64476
wangzijian1010 <wangyuyan1022@gmail.com>
2024-07-17 17:02:44 +0800

    add cv::mat to trt input method

--- lite/trt/core/trt_utils.cpp
+++ lite/trt/core/trt_utils.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "trt_utils.h"
+
+
+
+float* trtcv::utils::transform::create_tensor(const cv::Mat &mat,std::vector<int64_t> input_node_dims,unsigned int data_format){
+    // make mat to float type's vector
+
+    const unsigned int rows = mat.rows;
+    const unsigned int cols = mat.cols;
+    const unsigned int channels = mat.channels();
+
+    cv::Mat mat_ref;
+    if (mat.type() != CV_32FC(channels)) mat.convertTo(mat_ref, CV_32FC(channels));
+    else mat_ref = mat; // reference only. zero-time cost. support 1/2/3/... channels
+
+    if (input_node_dims.size() != 4) throw std::runtime_error("dims mismatch.");
+    if (input_node_dims.at(0) != 1) throw std::runtime_error("batch != 1");
+
+
+    if (data_format == transform::CHW)
+    {
+        const unsigned int target_tensor_size = rows * cols * channels;
+        // input vector's size
+        float* input_vector = new float [target_tensor_size];
+
+        for (int c = 0; c < channels; ++c)
+        {
+            for (int h = 0; h < rows; ++h)
+            {
+                for (int w = 0; w < cols; ++w)
+                {
+                    input_vector[c * rows * cols + h * cols + w] = mat.at<cv::Vec3f>(h, w)[c];
+                }
+            }
+        }
+        return input_vector;
+    }
+}
+
+
+
--- lite/trt/core/trt_utils.h
+++ lite/trt/core/trt_utils.h
@@ -4,11 +4,24 @@
 
 #ifndef LITE_AI_TOOLKIT_TRT_UTILS_H
 #define LITE_AI_TOOLKIT_TRT_UTILS_H
-
+#include "trt_config.h"
 
 namespace trtcv
 {
-    // no specific utils for trt now.
+    // specific utils for trt .
+    namespace utils
+    {
+        namespace transform
+        {
+            enum
+            {
+                CHW = 0, HWC =1
+            };
+            LITE_EXPORTS float* create_tensor(const cv::Mat &mat,std::vector<int64_t> input_node_dims,unsigned int data_format = CHW);
+
+
+        }
+    }
 }
 
 
