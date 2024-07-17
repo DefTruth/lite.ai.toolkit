71f3c73e9ddf9dc8ab182cea1571151906684432
wangzijian1010 <wangyuyan1022@gmail.com>
2024-07-17 18:27:47 +0800

    update test code

--- examples/lite/cv/test_lite_yolov8face.cpp
+++ examples/lite/cv/test_lite_yolov8face.cpp
@@ -31,9 +31,9 @@ static void test_default()
 static void test_tensorrt()
 {
 #ifdef ENABLE_TENSORRT
-    std::string engine_path = "../../../examples/hub/trt/cv/yoloface_8n.engine";
+    std::string engine_path = "../../../examples/hub/trt/cv/yoloface_fp16.engine";
     std::string test_img_path = "../../../examples/lite/resources/test_lite_yolov5_1.jpg";
-    std::string save_img_path = "../../../examples/logs/test_lite_yolov8face_trt.jpg";
+    std::string save_img_path = "../../../examples/logs/test_lite_yolov8face_trtbbbb.jpg";
 
     lite::trt::cv::face::detection::YOLOV8Face *yolov8_face  = new lite::trt::cv::face::detection::YOLOV8Face (engine_path);
 
@@ -44,6 +44,8 @@ static void test_tensorrt()
     yolov8_face->detect(test_image,detected_boxes,0.5f,0.4f);
 
     std::cout<<"trt face detect done!"<<std::endl;
+    lite::utils::draw_boxes_inplace(test_image, detected_boxes);
+    cv::imwrite(save_img_path, test_image);
 
     delete yolov8_face;
 #endif
