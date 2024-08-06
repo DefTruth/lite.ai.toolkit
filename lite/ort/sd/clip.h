bbeacc830b15d6d1c8fce2429219c3b982b7a55e
wangzijian <107230768+wangzijian1010@users.noreply.github.com>
2024-08-06 11:39:12 +0800

    [Misc] Remove un-need comments (#425)

--- lite/ort/sd/clip.h
+++ lite/ort/sd/clip.h
@@ -11,28 +11,6 @@
 #include "vector"
 #include "tokenizer.h"
 
-//namespace ortsd
-//{
-//    class LITE_EXPORTS Clip : public BasicOrtHandler
-//    {
-//    public:
-//        explicit Clip(const std::string &_onnx_path, unsigned int _num_threads = 1) :
-//                BasicOrtHandler(_onnx_path, _num_threads)
-//        {};
-//
-//        ~Clip() override = default;
-//
-//    public:
-//        void encode_text(std::vector<std::string> input_text,std::vector<int>& output);
-//
-//        void inference(std::vector<int> input,std::vector<float> &output);
-//
-//        void inference(std::vector<std::string> input,std::vector<float> &output);
-//
-//        Ort::Value transform(const cv::Mat &mat);
-//    };
-//}
-
 namespace ortsd
 {
     class  Clip
